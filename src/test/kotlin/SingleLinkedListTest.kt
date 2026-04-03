980f427c09fd8a69ef0921f981e41491f3240a41
maxicot <st140678@student.spbu.ru>
2026-04-03 20:59:26 +0300

    Implement additional tests

--- src/test/kotlin/SingleLinkedListTest.kt
+++ src/test/kotlin/SingleLinkedListTest.kt
@@ -33,6 +33,19 @@ class SinglyLinkedListTest {
     }
 
     @Test
+    fun `added elements are properly ordered`() {
+        list.add(1)
+        list.add(2)
+        list.addFirst(0)
+        list.add(3)
+
+        assertEquals(list[0], 0)
+        assertEquals(list[1], 1)
+        assertEquals(list[2], 2)
+        assertEquals(list[3], 3)
+    }
+
+    @Test
     fun `remove element`() {
         list.add(1)
         list.add(2)
@@ -64,6 +77,16 @@ class SinglyLinkedListTest {
     }
 
     @Test
+    fun `removed isn't contained`() {
+        list.add(1)
+        list.add(2)
+        list.remove(1)
+
+        assertFalse(list.contains(1))
+        assertTrue(list.contains(2))
+    }
+
+    @Test
     fun `get by index`() {
         list.add(10)
         list.add(20)
@@ -72,6 +95,17 @@ class SinglyLinkedListTest {
         assertEquals(10, list[0])
         assertEquals(20, list[1])
         assertEquals(30, list[2])
+    }
+
+    @Test
+    fun `removed can't be indexed`() {
+        list.add(0)
+        list.add(1)
+        list.remove(1)
+
+        assertThrows(IndexOutOfBoundsException::class.java) {
+            list[1]
+        }
     }
 
     @Test
@@ -84,6 +118,7 @@ class SinglyLinkedListTest {
         }
     }
 
+    @Test
     fun `indexOf works`() {
         list.add(10)
         list.add(20)
@@ -92,6 +127,15 @@ class SinglyLinkedListTest {
         assertEquals(2, list.indexOf(20))
     }
 
+    @Test
+    fun `removed can't be found`() {
+        list.add(0)
+        list.remove(0)
+
+        assertEquals(list.indexOf(0), -1)
+    }
+
+    @Test
     fun `set works`() {
         list.add(10)
         list.add(20)
@@ -99,5 +143,14 @@ class SinglyLinkedListTest {
 
         list[2] = 5
         assertEquals(5, list[2])
+    }
+
+    @Test
+    fun `iteration works`() {
+        list.add(1)
+        list.add(2)
+        list.add(3)
+
+        assertEquals(list.iterator().toList().toIntArray(), [1, 2, 3])
     }
 }
